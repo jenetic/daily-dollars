5e62e2470def83354b76024a95da0786d8b9520f
srnq <77878502+xzjenny@users.noreply.github.com>
2022-04-12 11:26:02 -0700

    Updated variables

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 // Note: Program counts the current day as one day.
 
 // Converts day into int value, with  as the first date in dateRanges array
-function daysFromSchoolStart(firstDay, date) {
+const daysFromSchoolStart = (firstDay, date) => {
     return (Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) - Date.UTC(firstDay.getFullYear(), firstDay.getMonth(), firstDay.getDate())) / 24 / 60 / 60 / 1000; 
 }
 
 // Takes list of normal date ranges and an empty list -> turns that empty list into a list of date ranges in int form
-function daysFromSchoolStartList(dateRanges, dateRangesInt) {
+const daysFromSchoolStartList = (dateRanges, dateRangesInt) => {
     for (let i = 0; i < dateRanges.length; i++) {
-        const row = [];
+        let row = [];
         row.push(daysFromSchoolStart(dateRanges[0][0], dateRanges[i][0])); // Start of range
         row.push(daysFromSchoolStart(dateRanges[0][0], dateRanges[i][1])); // End of range
         dateRangesInt.push(row);
@@ -16,7 +16,7 @@ function daysFromSchoolStartList(dateRanges, dateRangesInt) {
 }
 
 // Appends int school days to a list of school days
-function schoolDaysListFromRangesInt(dateRangesInt, schoolDaysList) {
+const schoolDaysListFromRangesInt = (dateRangesInt, schoolDaysList) => {
     for (let i = 0; i < dateRangesInt.length; i++){
         // Iternates through each day in within each range
         for (let j = dateRangesInt[i][0]; j <= dateRangesInt[i][1]; j++) {
@@ -26,15 +26,15 @@ function schoolDaysListFromRangesInt(dateRangesInt, schoolDaysList) {
 }
 
 // Format date into mm/dd/yyyy format (for display)
-function dateFormat(date) {
+const dateFormat = (date) => {
     return (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear();
 }
 
 // Calculate dollars left based on how many days of school are left
-function calculateDollars() {
+const calculateDollars = () => {
     
     // Store user input as variable "dollars"
-    var dollars = document.getElementById("dollars").value;
+    let dollars = document.getElementById("dollars").value;
     
     // Check if user inputted a negative number or something that isn't a number    
     if (dollars == "" || dollars < 0 || !(!isNaN(dollars) && !isNaN(parseFloat(dollars)))) {
@@ -49,12 +49,12 @@ function calculateDollars() {
         [new Date(2022, 0, 3), new Date(2022, 2, 19)], // Spring break
         [new Date(2022, 2, 28), new Date(2022, 5, 10)] 
     ];
-    var firstDay = dateRanges[0][0];
-    var lastDay = dateRanges[dateRanges.length - 1][1];
+    let firstDay = dateRanges[0][0];
+    let lastDay = dateRanges[dateRanges.length - 1][1];
     
     // CHECK IF BREAKS ARE INCLUDED
-    var includeWinterBreak = document.getElementById("includeWinterBreak").checked;
-    var includeSpringBreak = document.getElementById("includeSpringBreak").checked;
+    let includeWinterBreak = document.getElementById("includeWinterBreak").checked;
+    let includeSpringBreak = document.getElementById("includeSpringBreak").checked;
     
     // If both include winter and spring break are checked, edit dateRanges array to remove breaks
     if (includeWinterBreak && includeSpringBreak) {
@@ -78,9 +78,9 @@ function calculateDollars() {
     daysFromSchoolStartList(dateRanges, dateRangesInt);
     schoolDaysListFromRangesInt(dateRangesInt, schoolDaysList);
 
-    var today = new Date();
-    var todayUpdated = new Date();
-    var todayInt = daysFromSchoolStart(firstDay, today); // "todayInt" is int version of "today"
+    let today = new Date();
+    let todayUpdated = new Date();
+    let todayInt = daysFromSchoolStart(firstDay, today); // "todayInt" is int version of "today"
 
     // RECONFIGURE CURRENT DAY
     // If current day is before first day of school, set todayInt to 1st day of school
@@ -96,7 +96,7 @@ function calculateDollars() {
     }
 
     // Because the days before 1st and after last day of school are taken care of, any day that isn't in the list has to be on a break
-    var todayIndex = schoolDaysList.indexOf(todayInt);
+    let todayIndex = schoolDaysList.indexOf(todayInt);
 
     // If current day is during a break, sets current day to the first day of when school resumes
     if (todayIndex === -1) {
@@ -113,7 +113,7 @@ function calculateDollars() {
     }
 
     // Finds number of school days left, including the current day
-    var schoolDaysLeft = schoolDaysList.length - todayIndex;
+    let schoolDaysLeft = schoolDaysList.length - todayIndex;
 
     // EXCLUDE WEEKENDS
     if (document.getElementById("excludeWeekends").checked) {
@@ -128,7 +128,7 @@ function calculateDollars() {
         }
         
         // Find number of weekends
-        var weekendCounter = 0;
+        let weekendCounter = 0;
         for (let i = 0; i < dateRanges.length; i++) {
             let start = dateRanges[i][0];
             let end = dateRanges[i][1];
@@ -144,7 +144,7 @@ function calculateDollars() {
     }
 
     // Exclude additional days
-    var excludeDays = document.getElementById("excludeDays").value;
+    let excludeDays = document.getElementById("excludeDays").value;
     if (!excludeDays) { excludeDays = 0; }
 
     if (excludeDays % 1 != 0 || excludeDays < 0) {
@@ -160,7 +160,7 @@ function calculateDollars() {
     // Round user input to 2 decimal places
     dollars = (Math.round((dollars) * 1e2)/1e2).toFixed(2);
 
-    var result = "$" + (Math.round((dollars / (schoolDaysLeft - excludeDays)) * 1e2)/1e2).toFixed(2); //toFixed(2) adds extra 0s if the number has less than 2 decimal places
+    const result = "$" + (Math.round((dollars / (schoolDaysLeft - excludeDays)) * 1e2)/1e2).toFixed(2); //toFixed(2) adds extra 0s if the number has less than 2 decimal places
 
     // DISPLAYS RESULTS
     const resultsList = document.getElementsByClassName("results"); // getElementsByClasssNames returns an array
@@ -183,7 +183,7 @@ function calculateDollars() {
 }
   
 // Prevents page from refreshing when user clicks submit
-var form = document.getElementById("form");
+const form = document.getElementById("form");
 function handleForm(event) { event.preventDefault(); } 
 form.addEventListener('submit', handleForm);
 
